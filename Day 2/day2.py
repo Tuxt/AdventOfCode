6361988a8985cccca537e6ddf4d26d630d9a5709
Tuxt <wm.tuxt@gmail.com>
2021-02-07 06:21:30 +0100

    [2020] Day 2: Part 2

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import re
 
 input_file = 'input'
 
@@ -9,7 +10,7 @@ data = data.split('\n')[:-1]
 
 data = [ e.split(': ') for e in data ]
 
-def check_password(policy, password):
+def check_password1(policy, password):
     # FORMAT: "low-high char"
     policy, char = policy.split(' ')
     low, high  = policy.split('-')
@@ -17,9 +18,25 @@ def check_password(policy, password):
 
     return low <= Counter(password)[char] <= high
 
-result1 = sum([ check_password(e[0], e[1]) for e in data ])
+result1 = sum([ check_password1(e[0], e[1]) for e in data ])
 
 print('[DAY 2]: Part 1')
-print('Total valid passwords: {}'.format( result1 ))
+print('Total valid passwords: {}'.format(result1))
 
+
+
+def check_password2(policy, password):
+    # FORMAT: "low-high char"
+    policy, char = policy.split(' ')
+    pos1, pos2 = policy.split('-')
+    pos1, pos2 = int(pos1), int(pos2)
+
+    occurrences = [ match.start()+1 for match in re.finditer(char, password) ]
+
+    return (pos1 in occurrences) ^ (pos2 in occurrences)
+
+result2 = sum([ check_password2(e[0], e[1]) for e in data ])
+
+print('[DAY 2]: Part 2')
+print('Total valid passwords: {}'.format(result2))
 
