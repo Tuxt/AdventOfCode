616abc2d421436d3aedb8faee7813d0864a1f397
Tuxt <wm.tuxt@gmail.com>
2021-02-09 17:15:53 +0100

    [2020] Day 5: Part 2

--- Day 5/day5.py
+++ Day 5/day5.py
@@ -16,3 +16,28 @@ print('[DAY 5]: Part 1')
 print('Highest seat ID: {}'.format(max(seat_ids)))
 
 
+seat_ids = sorted(seat_ids)
+min_id = min(seat_ids)
+
+high_idx = len(seat_ids)
+low_idx = 0
+
+found = False
+while not found:
+    mid_idx = low_idx + (high_idx - low_idx)//2
+
+    if (high_idx - low_idx) < 2:
+        found = True
+
+    if seat_ids[mid_idx] == mid_idx + min_id:
+        low_idx = mid_idx
+    else:
+        high_idx = mid_idx
+
+current_id = int(seat_ids[mid_idx])
+above_id = int(seat_ids[mid_idx + 1])
+
+found_id = current_id + 1 if (above_id - current_id) == 2 else current_id - 1
+
+print('\n[DAY 5]: Part 2')
+print('My seat ID: {}'.format(found_id))
