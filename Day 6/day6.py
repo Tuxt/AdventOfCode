95d8e8fb807fec3f837f2e1fa09eafbfd116c47f
Tuxt <wm.tuxt@gmail.com>
2021-02-10 21:03:57 +0100

    [2020] Day 6: Part 2

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 input_file = 'input'
 
 with open(input_file, 'r') as f:
@@ -11,4 +13,16 @@ answered_yes = [ len( set( ''.join(e) ) ) for e in data ]
 
 print('[DAY 6]: Part 1')
 print('Sum of counts: {}'.format( sum(answered_yes) ))
+
+
+# Questions answered yes by everyone in the group
+
+# For every group (e in data), count answers for every question and get how many
+# was answered by all the group members (v == len(e))
+counters = [ sum([ v == len(e) for v in Counter(''.join(e)).values() ])  for e in data ]
+
+print('\n[DAY 6]: Part 2')
+print('Sum of counts: {}'.format( sum(counters) ))
+
+
 
