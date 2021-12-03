b7fe5f18f8de8f9d873a2ad52f0e717487dc7310
Tuxt <wm.tuxt@gmail.com>
2021-12-03 18:51:07 +0100

    [2021] Day 1: Part 2

--- 2021/Day 1/day1.py
+++ 2021/Day 1/day1.py
@@ -15,3 +15,11 @@ print('[DAY 1]: Part 1')
 print('Number of times a deph measurement increases: {}'.format(increments))
 
 
+sliding_windows = np.lib.stride_tricks.sliding_window_view(data, 3)
+
+sum_windows = np.sum(sliding_windows, axis=1)
+
+increments = np.sum((sum_windows[1:] - sum_windows[:-1]) > 0)
+
+print('[DAY 1]: Part 2')
+print('Number of times the sum of measurements in this sliding window increases: {}'.format(increments))
