018d71bf06e58b0605795eaffdae8c7541963110
Tuxt <wm.tuxt@gmail.com>
2021-12-08 16:21:30 +0100

    [2021] Day 8: Part 2

--- 2021/Day 8/day8.py
+++ 2021/Day 8/day8.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import difflib
 
 input_file = 'input'
 
@@ -13,4 +14,45 @@ print('[DAY 8]: Part 1')
 print('Times the digits 1, 4, 7 and 8 appear in output: {}'.format(decoded_output.isin(len2digits.values()).sum().sum()))
 
 
+def decode_5len(item, one, four):
+    if sum(map(lambda e: e.startswith(' '), list(difflib.ndiff(one, item)))) == 2:
+        return 3
+    elif sum(map(lambda e: e.startswith(' '), list(difflib.ndiff(four, item)))) == 2:
+        return 2
+    else:
+        return 5
 
+def decode_6len(item, one, four):
+    if sum(map(lambda e: e.startswith(' '), list(difflib.ndiff(four, item)))) == 4:
+        return 9
+    elif sum(map(lambda e: e.startswith(' '), list(difflib.ndiff(one, item)))) == 2:
+        return 0
+    else:
+        return 6
+
+def decode(signal, output):
+    try:
+        items_to_decode = output.where(output.str.isalpha()).dropna().apply(sorted)
+    except:
+        return
+
+    one, _, four, *_ = sorted(signal.apply(sorted), key=len)
+
+    for item in set(items_to_decode.str.join('')):
+        if len(item) == 5:
+            decoded_item = decode_5len(list(item), one, four)
+        elif len(item) == 6:
+            decoded_item = decode_6len(list(item), one, four)
+
+        items_to_decode[items_to_decode.str.join('') == item] = decoded_item
+
+        output[items_to_decode.index] = items_to_decode
+
+[ decode(signal.loc[i], decoded_output.loc[i]) for i in range(len(decoded_output)) ]
+
+decoded_output[0] *= 1000
+decoded_output[1] *= 100
+decoded_output[2] *= 10
+
+print('\n[DAY 8]: Part 2')
+print('Sum of all decoded values: {}'.format(decoded_output.sum().sum()))
