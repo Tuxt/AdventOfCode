b9c5392e5346b836bbd23c0bbc8552ad25ac2500
Tuxt <wm.tuxt@gmail.com>
2021-12-09 15:39:10 +0100

    [2021] Day 9: Part 2

--- 2021/Day 9/day9.py
+++ 2021/Day 9/day9.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import ndimage
+import cv2
 
 input_file = 'input'
 
@@ -16,3 +17,11 @@ low_values_mask = ndimage.generic_filter(data, is_low, size=(3, 3), mode='consta
 print('[DAY 9]: Part 1')
 print('Sum of the risk levels of all low points: {}'.format(np.sum(data[low_values_mask]+1)))
 
+low_coords_x, low_coords_y = low_values_mask.nonzero()
+limits = (data == 9).astype(int)
+
+basin_size = lambda arr, x, y: cv2.floodFill(arr, None, (y, x), 2)[0]
+basin_sizes = [basin_size(limits, x, y) for x, y in zip(low_coords_x, low_coords_y)]
+
+print('\n[DAY 9]: Part 2')
+print('Multiply together the sizes of the three largest basins: {}'.format(np.prod(sorted(basin_sizes)[-3:])))
