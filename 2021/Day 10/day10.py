6c84371ba23328bea6d8c5ee493f5b10438fb0c3
Tuxt <wm.tuxt@gmail.com>
2021-12-10 10:31:46 +0100

    [2021] Day 10: Part 2

--- 2021/Day 10/day10.py
+++ 2021/Day 10/day10.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 input_file = 'input'
 
 with open(input_file, 'r') as f:
@@ -12,17 +14,44 @@ ERROR_SCORE = {
 }
 
 
-def process_line(line):     # Faster than using stack, same as recursive
-    line_lenght = len(line)+1
-    while len(line) != line_lenght:
-        line_lenght = len(line)
-        line = line.replace('{}', '').replace('()', '').replace('[]', '').replace('<>', '')
+def remove_pairs(line):
+    line_length = len(line)
+    line = line.replace('{}', '').replace('()', '').replace('[]', '').replace('<>', '')
+    if line_length != len(line):
+        return remove_pairs(line)
+    return line
+
+
+def find_error(line):
+    line = remove_pairs(line)
     line = line.replace('(', '').replace('{', '').replace('[', '').replace('<', '')
     return line[0] if len(line) > 0 else None
 
 
-scores = [ERROR_SCORE[process_line(line)] for line in data]
+error_scores = [ERROR_SCORE[find_error(line)] for line in data]
+
 
 print('[DAY 10]: Part 1')
-print('Total syntax error score: {}'.format(sum(scores)))
+print('Total syntax error score: {}'.format(sum(error_scores)))
 
+
+incomplete = np.array(data)[np.array(error_scores) == 0]
+
+COMPLETION_SCORE = {
+    '(': 1,
+    '[': 2,
+    '{': 3,
+    '<': 4
+}
+
+
+def completion_score(line, score=0):
+    *line, current = line
+    score = score * 5 + COMPLETION_SCORE[current]
+    return score if len(line) == 0 else completion_score(line, score)
+
+
+completion_scores = [completion_score(remove_pairs(line)) for line in incomplete]
+
+print('\n[DAY 10]: Part 2')
+print('Middle score: {}'.format(np.median(completion_scores).astype(np.int64)))
