7b2312eed6791980884532bb4255eef0f04bbde9
Tuxt <wm.tuxt@gmail.com>
2021-12-12 17:26:46 +0100

    [2021] Day 12: Part 2

--- 2021/Day 12/day12.py
+++ 2021/Day 12/day12.py
@@ -12,28 +12,33 @@ for edge in data:
     node1, node2 = edge.split('-')
     paths[node1] = paths.get(node1, []) + [node2]
     paths[node2] = paths.get(node2, []) + [node1]
-    visits[node1] = 1 if node1.islower() else np.inf
-    visits[node2] = 1 if node2.islower() else np.inf
-visits['end'] = np.inf  # End cave is stopped in a particular case
+    visits[node1] = 0
+    visits[node2] = 0
 
 
-def search(node, current_visits, current_path=[], paths_found=[]):
-    current_path.append(node)
+def search(node, current_visits, paths_found=0, max_visits=1, max_small_caves=np.inf):
     if node == 'end':
-        paths_found.append(current_path.copy())
-        return
+        paths_found += 1
+        return paths_found
 
-    current_visits[node] -= 1
-    for path in paths[node]:
-        if current_visits[path] > 0:
-            search(path, current_visits, current_path, paths_found)
     current_visits[node] += 1
+    for path in paths[node]:
+        if path == 'start':
+            continue
+        if np.sum(np.array([v for k, v in visits.items() if k.islower()]) == max_visits) > max_small_caves:
+            break
+        if path.isupper() or current_visits[path] < max_visits:
+            paths_found = search(path, current_visits, paths_found, max_visits=max_visits, max_small_caves=max_small_caves)
+    current_visits[node] -= 1
     return paths_found
 
 
 paths_found = search('start', visits)
 
 print('[DAY 12]: Part 1')
-print('Paths in which the small caves are visited 1 time at most: {}'.format(len(paths_found)))
+print('Paths in which the small caves are visited 1 time at most: {}'.format(paths_found))
 
+paths_found = search('start', visits, max_visits=2, max_small_caves=1)
+print('\n[DAY 12]: Part 2')
+print('Paths in which one small cave are visited 2 times: {}'.format(paths_found))
 
