22d5e707bae40d540188fc2c3bb9a878f6773ac2
Tuxt <wm.tuxt@gmail.com>
2022-12-01 17:55:25 +0100

    [2022] Day 1: Part 2

--- 2022/Day 1/day1.py
+++ 2022/Day 1/day1.py
@@ -9,3 +9,6 @@ max_elves = [sum(elf) for elf in elves]
 
 print('[DAY 1]: Part 1')
 print('Total calories carried by the Elf with more calories: {}'.format(max(max_elves)))
+
+print('\n[DAY 1]: Part 2')
+print('Total calories carried by the 3 Elves with more calories: {}'.format(sum(sorted(max_elves, reverse=True)[:3])))
