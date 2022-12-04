adabc579c0500041e82b5bc96a016b7ffb4db660
Tuxt <wm.tuxt@gmail.com>
2022-12-04 05:11:16 +0100

    [2022] Day 3: Part 2

--- 2022/Day 3/day3.py
+++ 2022/Day 3/day3.py
@@ -10,3 +10,10 @@ priorities = map(lambda x: string.ascii_letters.find(x) + 1, items_in_both_compa
 print('[DAY 3]: Part 1')
 print('Sum of priorities: {}'.format(sum(priorities)))
 
+
+iter_group = iter(data)
+group_badges = [set(a).intersection(set(b)).intersection(set(c)).pop() for a, b, c in zip(*[iter_group]*3)]
+badges_priorities = map(lambda x: string.ascii_letters.find(x) + 1, group_badges)
+
+print('\n[DAY 3]: Part 2')
+print('Sum of badges priorities: {}'.format(sum(badges_priorities)))
