9863e13e0f691034e765608e1874169080501aa5
Tuxt <wm.tuxt@gmail.com>
2022-12-05 06:46:35 +0100

    [2022] Day 5: Part 2

--- 2022/Day 5/day5.py
+++ 2022/Day 5/day5.py
@@ -1,3 +1,5 @@
+import copy
+
 FILENAME = 'input'
 
 with open(FILENAME) as f:
@@ -11,6 +13,7 @@ num_columns = int(max(col_pos))
 stacks = [list(map(lambda x, i: x[i], drawing, [col_pos.find(str(col))] * num_columns)) for col in range(1, num_columns + 1)]
 stacks = [list(''.join(stack).strip()) for stack in stacks]     # Join to strip (spaces), and convert to list again
 [stack.reverse() for stack in stacks]                           # Reverse lists: top at the end (inplace operation)
+original_stacks = copy.deepcopy(stacks)
 
 # Transform commands: [num_crates, origin, destination]
 commands = [[int(val) for val in command.split() if val.isdigit()] for command in commands]
@@ -24,3 +27,15 @@ top_stacks = list(map(lambda x: x[-1], stacks))
 print('[DAY 5]: Part 1')
 print('Crates at top: {}'.format(''.join(top_stacks)))
 
+# Restore initial state
+stacks = original_stacks
+
+# Perform commands
+[[stacks[dest-1].append(stacks[origin-1].pop(-i)) for i in range(num_crates, 0, -1)] for (num_crates, origin, dest) in commands]
+
+# Get the top (last) element from each stack
+top_stacks = list(map(lambda x: x[-1], stacks))
+
+
+print('\n[DAY 5]: Part 2')
+print('Crates at top: {}'.format(''.join(top_stacks)))
