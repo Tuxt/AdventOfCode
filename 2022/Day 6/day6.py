9a6e19d3b423a658d24bd0958d17ecf73890715b
Tuxt <wm.tuxt@gmail.com>
2022-12-06 10:21:00 +0100

    [2022] Day 6: Part 2

--- 2022/Day 6/day6.py
+++ 2022/Day 6/day6.py
@@ -3,9 +3,13 @@ FILENAME = 'input'
 with open(FILENAME) as f:
     data = f.read().strip()
 
-quads_pos = {i: data[i-4: i] for i in range(4, len(data))}
-start_markers = [k for k, v in quads_pos.items() if len(set(v)) == 4]
+def find_markers(data, num_chars):
+    positions = {i: data[i-num_chars: i] for i in range(num_chars, len(data))}
+    start_markers = [k for k, v in positions.items() if len(set(v)) == num_chars]
+    return start_markers
 
 print('[DAY 6]: Part 1')
-print('First start marker: {}'.format(start_markers[0]))
+print('First start-of-packet marker: {}'.format(find_markers(data, 4)[0]))
 
+print('\n[DAY 6]: Part 2')
+print('First start-of-message marker: {}'.format(find_markers(data, 14)[0]))
