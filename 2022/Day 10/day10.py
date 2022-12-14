c42e3b3898e84cbe8e29dff5423b576c7bfd36a8
Tuxt <wm.tuxt@gmail.com>
2022-12-14 18:06:24 +0100

    [2022] Day 10: Part 2

--- 2022/Day 10/day10.py
+++ 2022/Day 10/day10.py
@@ -5,9 +5,8 @@ FILENAME = 'input'
 
 class Program:
 
-    cycle = 0
-    signal = [None]     # None for cycle 0
-    register = 1        # Register X
+    CRT_WIDTH = 40
+    CRT_HEIGHT = 6
 
     def __init__(self, program):
         if type(program) is str and os.path.isfile(program):
@@ -18,15 +17,25 @@ class Program:
         else:
             raise ValueError('Must be a list of instructions or filename')
 
+        self.cycle = 0
+        self.signal = [None]  # None for cycle 0
+        self.register = 1  # Register X
+        self.crt = ''
+
     def reset(self):
         self.cycle = 0
         self.signal = [None]
         self.register = 1
+        self.crt = ''
 
     def _log_signal(self):
         self.signal.append(self.cycle * self.register)
 
+    def _draw_pixel(self):
+        self.crt += '#' if abs(self.cycle % Program.CRT_WIDTH - self.register) <= 1 else '.'
+
     def _noop(self):
+        self._draw_pixel()
         self.cycle += 1
         self._log_signal()
 
@@ -46,9 +55,19 @@ class Program:
         for instruction in self.instructions:
             self.run_instruction(instruction)
 
+    def render_screen(self):
+        [
+            print(self.crt[line * Program.CRT_WIDTH: line * Program.CRT_WIDTH + Program.CRT_WIDTH])
+            for line in range(Program.CRT_HEIGHT)
+        ]
+
 
 program = Program(FILENAME)
 program.run()
 
 print('[DAY 10]: Part 1')
 print('Sum of the 6 signal strengths: {}'.format(sum([program.signal[i] for i in [20, 60, 100, 140, 180, 220]])))
+
+print('\n[DAY 10]: Part 2')
+print('Rendered screen:')
+program.render_screen()
