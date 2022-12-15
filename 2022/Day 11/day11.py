abe3b44d2b095d5cb32a02c05b5bf024bc1cd714
Tuxt <wm.tuxt@gmail.com>
2022-12-15 16:33:00 +0100

    [2022] Day 11: Part 2

--- 2022/Day 11/day11.py
+++ 2022/Day 11/day11.py
@@ -6,12 +6,17 @@ with open(FILENAME) as f:
 
 class Monkey:
 
+    WORRY_DIVISION = 3
+    MAX_VALUE = 1
+
     @classmethod
     def build_monkey(cls, monkeys, monkey):
         monkey = monkey.strip().splitlines()
         items = [int(item.strip()) for item in monkey[1].strip().split(': ')[1].split(',')]
-        operation = lambda old: eval(monkey[2].split(' = ')[1]) // 3
-        test = lambda e: int(monkey[4].split()[-1]) if e % int(monkey[3].split()[-1]) == 0 else int(monkey[5].split()[-1])
+        operation = lambda old: eval(monkey[2].split(' = ')[1]) // Monkey.WORRY_DIVISION
+        divisible = int(monkey[3].split()[-1])
+        Monkey.MAX_VALUE *= divisible
+        test = lambda e: int(monkey[4].split()[-1]) if e % divisible == 0 else int(monkey[5].split()[-1])
         return Monkey(monkeys, items, operation, test)
 
     def __init__(self, monkeys, items, operation, test):
@@ -22,7 +27,7 @@ class Monkey:
         self.inspected_items = 0
 
     def _inspect_item(self):
-        self.items[0] = self.operation(self.items[0])
+        self.items[0] = self.operation(self.items[0]) % Monkey.MAX_VALUE
         self.inspected_items += 1
 
     def _throw_item(self):
@@ -38,15 +43,32 @@ class Monkey:
             self.process_item()
 
 
-# Instance monkeys
-monkeys = []
-[monkeys.append(Monkey.build_monkey(monkeys, monkey)) for monkey in data]
+if __name__ == '__main__':
+    # Instance monkeys
+    monkeys = []
+    [monkeys.append(Monkey.build_monkey(monkeys, monkey)) for monkey in data]
 
-# Run 20 rounds
-[monkey.process_all_items() for _ in range(20) for monkey in monkeys]
+    # Run 20 rounds
+    [monkey.process_all_items() for _ in range(20) for monkey in monkeys]
 
-# Get the inspected items for each monkey (sorted)
-inspected_items = sorted(map(lambda e: e.inspected_items, monkeys), reverse=True)
+    # Get the inspected items for each monkey (sorted)
+    inspected_items = sorted(map(lambda e: e.inspected_items, monkeys), reverse=True)
 
-print('[DAY 11]: Part 1')
-print('Monkey business: {}'.format(inspected_items[0] * inspected_items[1]))
+    print('[DAY 11]: Part 1')
+    print('Monkey business: {}'.format(inspected_items[0] * inspected_items[1]))
+
+
+    Monkey.WORRY_DIVISION = 1
+    Monkey.MAX_VALUE = 1
+    # Instance monkeys
+    monkeys = []
+    [monkeys.append(Monkey.build_monkey(monkeys, monkey)) for monkey in data]
+
+    # Run 10_000 rounds
+    [monkey.process_all_items() for _ in range(10_000) for monkey in monkeys]
+
+    # Get the inspected items for each monkey (sorted)
+    inspected_items = sorted(map(lambda e: e.inspected_items, monkeys), reverse=True)
+
+    print('[DAY 11]: Part 2')
+    print('Monkey business: {}'.format(inspected_items[0] * inspected_items[1]))
